673e49575992e6c1e907e155b9c303d61f6f78d3
Garrett <gart@gart.sh>
2024-10-29 17:53:19 -0400

    Update jpxs.lua

--- jpxs.lua
+++ jpxs.lua
@@ -2,10 +2,10 @@ local plugin = ...
 
 --
 -- jpxs is now entirely loaded from the asset server!
--- for config options and things, please refer to the .jpxs/config.json file
+-- for config options and things, please refer to the `jpxs config` command 
 --
 -- the plugin is now entirely open source!
--- you can download the plugin from https://assets.jpxs.io/plugins/jpxs/
+-- you can download the plugin from https://github.com/jpxs-intl/jpxs-plugin
 --
 -- if you have any questions, feel free to DM me on discord @gart
 -- enjoy! :3
